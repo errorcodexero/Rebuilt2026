ca0006fd9529be3615a8401a84129be834c66ee1
singhsharmadev-png <singhsharmadev-png>
2026-01-24 11:19:28 -0800

    recent

--- src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
@@ -13,32 +13,37 @@ import edu.wpi.first.units.measure.Current;
 import frc.robot.generated.CompTunerConstants;
 
 import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.ParentDevice;
 
 
 public final class IntakeIOHardware implements IntakeIO {
 
     private final TalonFX rollerMotor;
     private final TalonFX pivotMotor;
+    private StatusSignal<Angle> pivotAngleSignal;
+    private StatusSignal<AngularVelocity> pivotAngularVelocitySignal;
+    private StatusSignal<Angle> rollerAngleSignal;
+    private StatusSignal<AngularVelocity> rollerAngularVelocitySignal;
+    private StatusSignal<Voltage> rollerAppliedVoltsSignal;
+    private StatusSignal<Voltage> pivotAppliedVoltsSignal;
+    private StatusSignal<Current> rollerCurrentAmpsSignal;
+    private StatusSignal<Current> pivotCurrentAmpsSignal;
 
-    private StatusSignal <Angle> pivotAngleSignal;
-    private StatusSignal <AngularVelocity> pivotAngularVelocitySignal;
-    private StatusSignal <Angle> rollerAngleSignal;
-    private StatusSignal <AngularVelocity> rollerAngularVelocitySignal;
-    private StatusSignal <Voltage> rollerAppliedVoltsSignal;
-    private StatusSignal <Voltage> pivotAppliedVoltsSignal;
-    private StatusSignal <Current> rollerCurrentAmpsSignal;
-    private StatusSignal <Current> pivotCurrentAmpsSignal;
+    // Control requests
+    private final VoltageOut rollerVoltageRequest = new VoltageOut(0);
+    private final VoltageOut pivotVoltageRequest = new VoltageOut(0);
+    private final PositionVoltage pivotPositionRequest = new PositionVoltage(0.0);
 
     public IntakeIOHardware() {
-        rollerMotor = new TalonFX(IntakeConstants.rollerMotorCANID);
-        pivotMotor = new TalonFX(IntakeConstants.pivotMotorCANID);
+    rollerMotor = new TalonFX(IntakeConstants.rollerMotorCANID, CompTunerConstants.kCANBus);
+    pivotMotor = new TalonFX(IntakeConstants.pivotMotorCANID, CompTunerConstants.kCANBus);
         var limitConfigsPivot = new CurrentLimitsConfigs();
         limitConfigsPivot.SupplyCurrentLimit = IntakeConstants.currentLimit;
         limitConfigsPivot.SupplyCurrentLimitEnable = true;
@@ -49,12 +54,12 @@ public final class IntakeIOHardware implements IntakeIO {
         limitConfigsRoller.SupplyCurrentLimitEnable= true;
         rollerMotor.getConfigurator().apply(limitConfigsRoller);
 
-        /*Slot0Configs pivotSlot0Configs = new Slot0Configs();
+        Slot0Configs pivotSlot0Configs = new Slot0Configs();
         pivotSlot0Configs.kP = IntakeConstants.pivotKP;
         pivotSlot0Configs.kD= IntakeConstants.pivotKD;
         pivotSlot0Configs.kV= IntakeConstants.pivotKV;
         pivotSlot0Configs.kI= IntakeConstants.pivotKI;
-        pivotMotor.getConfigurator().apply(pivotSlot0Configs);*/
+        pivotMotor.getConfigurator().apply(pivotSlot0Configs);
 
         Slot0Configs rollerSlot0Configs= new Slot0Configs();
         rollerSlot0Configs.kP= IntakeConstants.rollerKP;
@@ -75,12 +80,14 @@ public final class IntakeIOHardware implements IntakeIO {
         MotionMagicConfigsPivot.MotionMagicAcceleration= 0; 
         pivotMotor.getConfigurator().apply(MotionMagicConfigsPivot);
 
-        public void updateInputs(IntakeIOInputs inputs) {
-            inputs.PivotAngle= pivotAngleSignal.getValue();
-        }
         
 
+        // Configure periodic frames
+        BaseStatusSignal.setUpdateFrequencyForAll(50.0, rollerAngularVelocitySignal, rollerAppliedVoltsSignal, rollerCurrentAmpsSignal, pivotAngularVelocitySignal, pivotAppliedVoltsSignal, pivotCurrentAmpsSignal);
+        BaseStatusSignal.setUpdateFrequencyForAll(20.0, rollerAngleSignal, pivotAngleSignal);
 
-
+        // Optimize CAN bus for these parent devices
+        ParentDevice.optimizeBusUtilizationForAll(rollerMotor, pivotMotor);
     }
+
 }
