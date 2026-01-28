359c15f9c5ae2f684c6492461a37ee2182c27830
pranay kollu <kollup1@wvhs.wlwv.k12.or.us>
2026-01-27 20:39:11 -0800

    finished climberconstants and started climber.io

---
+++ src/main/java/frc/robot/climber/climber.java
@@ -0,0 +1,7 @@
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+package.frc.robot.subsystems.climber;
+// imports needed for the climber  
+
+public class Climber extends SubsystemBase {
+   
+}
---
+++ src/main/java/frc/robot/climber/climberconstants.java
@@ -0,0 +1,16 @@
+package frc.robot.climber;
+
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import edu.wpi.first.units.measure.MomentOfInertia;
+
+public class climberconstants {
+
+public static final int MotoroneID = 0;
+public static final int MotortwoID = 1; 
+
+    public static class sim {
+        public static final MomentOfInertia
+        MOI=  KilogramSquareMeters.zero();
+        public static final double gearatio =1.0; 
+    }
+}
