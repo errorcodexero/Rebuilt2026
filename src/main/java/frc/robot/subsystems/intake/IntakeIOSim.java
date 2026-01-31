38a255b4a1624e33c8928aea5f54b7652265c599
singhsharmadev-png <singhsharmadev-png>
2026-01-31 10:04:05 -0800

    Pushed

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.generated.CompTunerConstants;
+
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.LinearSystemId;
@@ -8,22 +10,30 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import static edu.wpi.first.units.Units.KilogramSquareMeters;
 
 
-:
+
 public class IntakeIOSim extends IntakeIOHardware{
     private final DCMotorSim pivotMotorSim;
     private final DCMotorSim rollerMotorSim;
-
-    public IntakeIOSim() throws Exception{
-        final pivotMotorSim  = new DCMotorSim(
+    public IntakeIOSim() {
+        super(CompTunerConstants.kCANBus, CompTunerConstants.kCANBus);
+        pivotMotorSim  = new DCMotorSim(
             LinearSystemId.createDCMotorSystem(
                 DCMotor.getKrakenX60Foc(1), IntakeConstants.PIVOT_MOMENTOFINERTIA.in(KilogramSquareMeters), IntakeConstants.pivotGearRatio
             ),
-        DCMotor.getKrakenX60Foc(1)
+        DCMotor.getKrakenX60Foc(1) //Not sure how many motors
         );  
 
-        final rollerMotorSim= new DCMotorSim(
+        rollerMotorSim= new DCMotorSim(
             LinearSystemId.createDCMotorSystem(
-                DCMotor.getKrakenX60Foc(1), IntakeConstants., 0)
-        )
+                DCMotor.getKrakenX60Foc(1), IntakeConstants.ROLLER_MOMENTOFINERTIA.in(KilogramSquareMeters), IntakeConstants.rollerGearRatio
+            ),
+        DCMotor.getKrakenX60Foc(1) //Not sure how many motors
+        );
+    }
+
+    @Override
+    public void updateInputs(IntakeIOInputsAutoLogged inputs){
+        TalonFXSimState pivotMotorSimState= pivotMotor.getSimState();
+        rollerMotorSim= rollerMotorSim.getSimState();
     }
 }
