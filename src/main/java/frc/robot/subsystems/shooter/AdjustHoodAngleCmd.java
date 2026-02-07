275436c2df16e6e52f7ca21cfb3ef4e7632cef58
Naman Pradhan <s2gnaman@gmail.com>
2026-02-07 13:55:57 -0800

    added adjust hood angle cmd

--- src/main/java/frc/robot/subsystems/shooter/AdjustHoodAngleCmd.java
+++ src/main/java/frc/robot/subsystems/shooter/AdjustHoodAngleCmd.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems.shooter;
 
-public class AdjustHoodAngle {
+import static edu.wpi.first.units.Units.Degrees;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AdjustHoodAngleCmd extends Command {
     
+    private final Shooter shooter;
+    private final Angle delta;
+    
+    public AdjustHoodAngleCmd(Shooter shooter, Angle delta) {
+        super();
+        this.shooter = shooter;
+        this.delta = delta;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void initialize() {
+        Angle current = shooter.getHoodAngle();
+        if (current == null) {
+            current = Degrees.zero();
+        }
+
+        double targetDeg = current.plus(delta).in(Degrees);
+        double clampedDeg = Math.max(ShooterConstants.SoftwareLimits.hoodMinAngle, 
+        Math.min(ShooterConstants.SoftwareLimits.hoodMaxAngle, targetDeg));
+
+        shooter.hoodToPosCmd(Degrees.of(clampedDeg)).schedule();
+    }
 }
