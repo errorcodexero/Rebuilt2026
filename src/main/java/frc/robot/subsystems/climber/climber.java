6814dc9522ef1681956bae97573d3cff4a92e125
singhsharmadev-png <singhsharmadev-png>
2026-02-14 13:28:57 -0800

    started constants

--- src/main/java/frc/robot/subsystems/climber/climber.java
+++ src/main/java/frc/robot/subsystems/climber/climber.java
@@ -6,16 +6,15 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.climber.climberIO.ClimberInputs;
-import frc.robot.climber.climberIO.ClimberOutputs;
 
 
-public class climber extends SubsystemBase {
+
+public class Climber extends SubsystemBase {
     
     private final climberIO IO;
     private final ClimberInputs inputs = new ClimberInputs();
 
-     public climber(climberIO io) {
+     public Climber(climberIO io) {
         this.IO=io;
     }
 
--- src/main/java/frc/robot/subsystems/climber/climberconstants.java
+++ src/main/java/frc/robot/subsystems/climber/climberconstants.java
@@ -1,16 +1,17 @@
-package frc.robot.climber;
+package frc.robot.subsystems.climber;
 
 import static edu.wpi.first.units.Units.KilogramSquareMeters;
 import edu.wpi.first.units.measure.MomentOfInertia;
 
-public class climberconstants {
+public class ClimberConstants {
 
-public static final int MotoroneID = 0;
-public static final int MotortwoID = 1; 
+    public static final int deployMotorID = 0;
+    public static final int climbMotorID = 1; 
 
-    public static class sim {
-        public static final MomentOfInertia
-        MOI=  KilogramSquareMeters.zero();
-        public static final double gearatio =0.0; 
-    }
+    public static final double KP= 0.0; //For now
+    
+
+
+    public static final MomentOfInertia MOI=  KilogramSquareMeters.zero();
+    public static final double gearatio =0.0; 
 }
