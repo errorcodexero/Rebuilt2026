9efdd5cc80a9f40311667768d4691b2a350b92a5
singhsharmadev-png <singhsharmadev-png>
2026-02-14 13:30:52 -0800

    constants

--- src/main/java/frc/robot/subsystems/climber/climberconstants.java
+++ src/main/java/frc/robot/subsystems/climber/climberconstants.java
@@ -5,12 +5,22 @@ import edu.wpi.first.units.measure.MomentOfInertia;
 
 public class ClimberConstants {
 
-    public static final int deployMotorID = 0;
-    public static final int climbMotorID = 1; 
+    public static final int deployMotorID = 0; //For now
+    public static final int climbMotorID = 1;  //For now
 
-    public static final double KP= 0.0; //For now
+    public static final double deployKP= 0.0; //For now
+    public static final double deployKI= 0.0; //For now
+    public static final double deployKD= 0.0; //For now
+    public static final double deployKS= 0.0; //For now
+    public static final double deployKV= 0.0; //For now
+    public static final double deployKG= 0.0; //For now
     
-
+    public static final double climbKP= 0.0; //For now
+    public static final double climbKI= 0.0; //For now
+    public static final double climbKD= 0.0; //For now
+    public static final double climbKS= 0.0; //For now
+    public static final double climbKV= 0.0; //For now
+    public static final double climbKG= 0.0; //For now
 
     public static final MomentOfInertia MOI=  KilogramSquareMeters.zero();
     public static final double gearatio =0.0; 
