0f3dcee680789d69569198c3ed2cfe81f9cb938d
Anshu Mukherjee <ammukh20@gmail.com>
2026-02-14 22:53:07 -0800

    Added Trench to Trench Auto

---
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.drive.DriveCommands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.hopper.Hopper;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class AutoCommands {
+
+
+    public static Command NeutralZoneTrenchToTrench(Drive drive, IntakeSubsystem intake, Hopper hopper, Shooter shooter){
+        return Commands.sequence(
+            Commands.runOnce(() -> drive.resetGyroCmd()),
+
+            DriveCommands.initialFollowPathCommand(drive,"TopToBottom Trench").deadlineFor(intake.intakeSequence()),
+
+            shooter.shootCmd(hopper).withTimeout(3.8),
+        
+            DriveCommands.followPathCommand("BottomToTop").deadlineFor(intake.intakeSequence()),
+
+            shooter.shootCmd(hopper).withTimeout(2.5)
+        );
+    }
+    
+}
