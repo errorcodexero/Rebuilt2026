943e43ef82486d640354b7a22db79f83d7998c51
Pranav <pranav.kausikbs@gmail.com>
2026-02-16 18:47:58 -0800

    Working on adding straight path to test for path following

--- src/main/java/frc/robot/commands/drive/autos/AutoCommands.java
+++ src/main/java/frc/robot/commands/drive/autos/AutoCommands.java
@@ -39,4 +39,7 @@ public class AutoCommands {
             climb.toggle()
         ); 
     }
+    public static Command StraightLine(Drive drive){
+        return DriveCommands.followPathCommand("Straight Line");
+    }
 }
