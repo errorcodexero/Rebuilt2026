715f24d69be1f3c19561bdcdc401477e784f12f7
Pranav <pranav.kausikbs@gmail.com>
2026-02-18 07:17:44 -0800

    Testing red

--- src/main/java/frc/robot/commands/drive/autos/AutoCommands.java
+++ src/main/java/frc/robot/commands/drive/autos/AutoCommands.java
@@ -15,25 +15,25 @@ public class AutoCommands {
     public static Command NZClimbAuto(Drive drive, Shooter shooter, IntakeSubsystem intake, Hopper hopper, ThriftyClimb climb, Angle angle ){
         return Commands.sequence(
             //Drive to the first shooting position and shoot preloaded balls   
-            DriveCommands.initialFollowPathCommand(drive, "Start to Shoot1"),
+            DriveCommands.initialFollowPathCommand(drive, "Start to Shoot1", false),
             shooter.shootCmd(hopper).withTimeout(1),
 
             //Drive to the collect position in the NZ and collect the ball s while moving, bringing down the hood to clear the trench
             
             Commands.sequence(
                 Commands.parallel(
-                    DriveCommands.followPathCommand("Shoot1 to Collect"),
+                    DriveCommands.followPathCommand("Shoot1 to Collect", false),
                     shooter.hoodToPosCmd(angle)
                 ),
 
                 //Drive to the second shooting position and shooting
-                DriveCommands.followPathCommand("Collect to Shoot2")
+                DriveCommands.followPathCommand("Collect to Shoot2", false)
             ).deadlineFor(intake.intakeSequence()),
 
             shooter.shootCmd(hopper).withTimeout(4),
             
             //Drive to the climb position, then climb
-            DriveCommands.followPathCommand("Shoot2 to Climb"),
+            DriveCommands.followPathCommand("Shoot2 to Climb", false),
             climb.toggle(),
             Commands.waitSeconds(1),
             climb.toggle()
