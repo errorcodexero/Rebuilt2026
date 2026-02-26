4096400e088759fa99ed025d91cd93a6df415ee4
Michael <michael.r.candrian@gmail.com>
2026-02-26 15:06:30 -0800

    removed unused imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -52,8 +52,6 @@ import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterIO;
 import frc.robot.subsystems.shooter.ShooterIOSim;
 import frc.robot.subsystems.shooter.ShooterIOTalonFX;
-import frc.robot.subsystems.thriftyclimb.ThriftyClimb;
-import frc.robot.subsystems.thriftyclimb.ThriftyClimbIO;
 import frc.robot.subsystems.vision.AprilTagVision;
 import frc.robot.subsystems.vision.CameraIO;
 import frc.robot.subsystems.vision.CameraIOLimelight4;
@@ -70,7 +68,6 @@ public class RobotContainer {
     private IntakeSubsystem intake_;
     private Shooter shooter_;
     private Hopper hopper_;
-    private ThriftyClimb climb_;
 
     private CANBus roborioCANBus = new CANBus();
 
