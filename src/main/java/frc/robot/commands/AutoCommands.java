11237eb398816df8ebf9c9cd249886646d324ca6
Butch Griffin <butchg@comcast.net>
2026-03-05 12:55:47 -0800

    updated paths to start testing, set robot width/height/weight/module placements

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.drive.DriveCommands;
 import frc.robot.commands.robot.RobotCommands;
+import frc.robot.commands.robot.StartupCmd;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.hopper.Hopper;
 import frc.robot.subsystems.intake.IntakeSubsystem;
@@ -12,7 +13,11 @@ import frc.robot.subsystems.shooter.Shooter;
 public class AutoCommands {
     public static Command NeutralZoneTrenchToTrench(Drive drive, IntakeSubsystem intake, Hopper hopper, Shooter shooter){
         return Commands.sequence(
-            DriveCommands.initialFollowPathCommand(drive,"TopToBottom Trench").deadlineFor(intake.intakeSequence()),
+            Commands.parallel(
+                // Added: startup sequence to deploy intake and move balls from shooter to hopper
+                new StartupCmd(intake, hopper, shooter),
+                DriveCommands.initialFollowPathCommand(drive,"TopToBottom Trench").deadlineFor(intake.intakeSequence())
+            ),
 
             RobotCommands.shoot(shooter, hopper, intake, drive),
             DriveCommands.followPathCommand("BottomToTop").deadlineFor(intake.intakeSequence()),
