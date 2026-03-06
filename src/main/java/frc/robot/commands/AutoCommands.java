2f95b52e1823a70f5c8487bcafd849702391a73e
Pranav <pranav.kausikbs@gmail.com>
2026-03-05 16:09:23 -0800

    Added startup cmd to trench auto

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -8,11 +8,15 @@ import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.hopper.Hopper;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.commands.robot.StartupCmd;
 
 public class AutoCommands {
     public static Command NeutralZoneTrenchToTrench(Drive drive, IntakeSubsystem intake, Hopper hopper, Shooter shooter){
         return Commands.sequence(
-            DriveCommands.initialFollowPathCommand(drive,"TopToBottom Trench").deadlineFor(intake.intakeSequence()),
+            Commands.parallel(
+                DriveCommands.initialFollowPathCommand(drive,"TopToBottom Trench").deadlineFor(intake.intakeSequence()),
+                new StartupCmd(intake, hopper, shooter)
+            ),
 
             RobotCommands.shoot(shooter, hopper, intake, drive),
             DriveCommands.followPathCommand("BottomToTop").deadlineFor(intake.intakeSequence()),
