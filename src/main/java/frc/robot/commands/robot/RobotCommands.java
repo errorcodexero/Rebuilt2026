e0bab6f941da3f52829046887916c8eada9b9389
Michael <michael.r.candrian@gmail.com>
2026-03-17 14:11:57 -0700

    Revert "updated to bryce's better logic"
    This reverts commit 57a2abce08b22efedac94042b74f4d13d6aaf53d.

--- src/main/java/frc/robot/commands/robot/RobotCommands.java
+++ src/main/java/frc/robot/commands/robot/RobotCommands.java
@@ -36,16 +36,12 @@ public class RobotCommands {
             () -> drive.rotationIsNear(RobotState.rotationToHub(), ShooterConstants.aimingTolerance);
 
         return Commands.parallel(
-            DriveCommands.joystickDriveAtAngle(
-                () -> RobotState.rotationToHub()
-            ).until(() -> 
-                aimedAtHub.getAsBoolean() || 
-                DriveCommands.getLinearVelocityFromJoysticks().getNorm() == 0.0
-            ).andThen(drive.stopWithXCmd(), drive.idle().onlyWhile(() -> 
-                aimedAtHub.getAsBoolean() || 
-                DriveCommands.getLinearVelocityFromJoysticks().getNorm() == 0.0    
-            ))
-            .repeatedly(),
+            Commands.repeatingSequence(
+                DriveCommands.joystickDriveAtAngle(
+                    () -> RobotState.rotationToHub()
+                ).onlyWhile(() -> !aimedAtHub.getAsBoolean() || DriveCommands.getLinearVelocityFromJoysticks().getNorm() != 0.0), 
+                Commands.runOnce(drive::stopWithX, drive)
+            ),
             Commands.repeatingSequence(
                 Commands.waitUntil(aimedAtHub).deadlineFor(shooter.spinUpForDistance(RobotState::hubDistance)),
                 shooter.shootAtDistance(RobotState::hubDistance, hopper, intake)
