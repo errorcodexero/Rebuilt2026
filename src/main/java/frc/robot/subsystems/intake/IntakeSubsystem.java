e416079b4c1e13bb7816a6cbb36f8e93227a54e4
Michael <michael.r.candrian@gmail.com>
2026-03-19 13:52:24 -0700

    Revert "change shake to always stay up"
    This reverts commit d0d6eec727aac86ac77caea7c96d62aa628dc85c.

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -13,7 +13,6 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
@@ -210,7 +209,7 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public Command shakeBalls() {
-        return Commands.sequence(Commands.waitTime(IntakeConstants.timeBeforeShake), Commands.run(this::waiting, this))
+        return new MoveIntakeCmd(this, IntakeConstants.deployedAngle, IntakeConstants.waitingAngle, IntakeConstants.angleChangeDelay, IntakeConstants.angleDownDelay)
             .finallyDo(interrupted -> deploy());
     }
 
